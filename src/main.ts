fc856813d982c1871c95a8867c5854ae3cc51582
jlazaro <jlazaro135@gmail.com>
2024-06-16 23:48:22 +0200

    se  habilita cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://profilegpt.netlify.app', // Permite este origen
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
-    credentials: true, // Si necesitas soportar cookies
-  });
+  app.enableCors();
 
   await app.listen(process.env.PORT || 3000);
 }
