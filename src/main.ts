bcf21180d06cd450da41cce166609011170baade
jlazaro <jlazaro135@gmail.com>
2024-09-01 12:15:50 +0200

    localhost enabled to avoid CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://profilegpt.netlify.app', // Permite este origen
+    origin: ['https://profilegpt.netlify.app', 'http://localhost:4200'], // Permite este origen
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     credentials: true, // Si necesitas soportar cookies
   });
